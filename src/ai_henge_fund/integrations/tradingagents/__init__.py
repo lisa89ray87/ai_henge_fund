8c79376e1a63abe0f9ad806d2c355c541c6af028
lisa89ray87 <raymond87tan@gmail.com>
2026-08-14 23:35:14 +0800

    Add TradingAgents integration boundary

---
+++ src/ai_henge_fund/integrations/tradingagents/__init__.py
@@ -0,0 +1,5 @@
+"""TradingAgents integration boundary for AI Henge Fund."""
+
+from .adapter import TradingAgentsAdapter, TradingAgentsDecision
+
+__all__ = ["TradingAgentsAdapter", "TradingAgentsDecision"]
